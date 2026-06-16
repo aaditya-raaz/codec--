5aa310630fa4452ddcdbc5e5ab22bab07fe11fbb
Aditya Raj <adityaraj56558@gmail.com>
2026-06-17 00:01:18 +0530

    .

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-long long int bianrysearch(int n){
+long long int sqrtInteger(int n){
     int s = 0;
     int e = n;
     long long int ans = -1;
@@ -22,11 +22,24 @@ long long int bianrysearch(int n){
     return ans;
 }
 
-int main(){
-    int sqrt;
-    cout<<" Enter the number: ";
-    cin>>sqrt;
+double morePrecision(int n, int precision, int solution){
+    double factor = 1;
+    double ans = solution;
+    for(int i=0;  i<precision; i++){
+        factor= factor/10;
+        for(double j=ans; j*j<n; j=j+factor){
+            ans = j;
+        }
+    }
+    return ans;
+}
 
-    cout<<" The sqrt is "<<bianrysearch(sqrt)<<endl;
+int main(){
+    int n;
+    cout<<" Enter the number: ";
+    cin>>n;
+    int solution = sqrtInteger(n);
+
+    cout<<" The sqrt is "<<morePrecision(n,3,solution)<<endl;
     return 0;
 }
